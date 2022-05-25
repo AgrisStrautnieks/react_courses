2719f1f3ce56db7a3372c9889c3d1bc4ff9d6da9
AgrisStrautnieks <62353995+AgrisStrautnieks@users.noreply.github.com>
2022-05-25 23:48:07 +0300

    Initial commit

---
+++ src/component/Expenses/Expense.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expense = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.expenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expense;
